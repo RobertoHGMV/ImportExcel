e40e5755ee1e87fd2d4607e68bd55cd5df7ce8e4
Roberto <robertohgm@hotmail.com>
2017-12-13 23:05:32 -0200

    Importador de documento excel

---
+++ ImportExcel/ImporterForm.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace ImportExcel
+{
+    partial class ImporterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridImporter = new System.Windows.Forms.DataGridView();
+            this.cmdOk = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbQtdLines = new System.Windows.Forms.Label();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.cbSheet = new System.Windows.Forms.ComboBox();
+            this.btnImporter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gridImporter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridImporter
+            // 
+            this.gridImporter.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gridImporter.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridImporter.Location = new System.Drawing.Point(16, 47);
+            this.gridImporter.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gridImporter.Name = "gridImporter";
+            this.gridImporter.Size = new System.Drawing.Size(933, 358);
+            this.gridImporter.TabIndex = 0;
+            // 
+            // cmdOk
+            // 
+            this.cmdOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.cmdOk.Location = new System.Drawing.Point(16, 457);
+            this.cmdOk.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmdOk.Name = "cmdOk";
+            this.cmdOk.Size = new System.Drawing.Size(100, 28);
+            this.cmdOk.TabIndex = 2;
+            this.cmdOk.Text = "OK";
+            this.cmdOk.UseVisualStyleBackColor = true;
+            this.cmdOk.Click += new System.EventHandler(this.cmdOk_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 409);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Linhas:";
+            // 
+            // lbQtdLines
+            // 
+            this.lbQtdLines.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lbQtdLines.AutoSize = true;
+            this.lbQtdLines.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbQtdLines.Location = new System.Drawing.Point(104, 409);
+            this.lbQtdLines.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbQtdLines.Name = "lbQtdLines";
+            this.lbQtdLines.Size = new System.Drawing.Size(18, 20);
+            this.lbQtdLines.TabIndex = 5;
+            this.lbQtdLines.Text = "#";
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(16, 15);
+            this.txtFileName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.ReadOnly = true;
+            this.txtFileName.Size = new System.Drawing.Size(412, 22);
+            this.txtFileName.TabIndex = 8;
+            // 
+            // cbSheet
+            // 
+            this.cbSheet.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbSheet.FormattingEnabled = true;
+            this.cbSheet.Location = new System.Drawing.Point(740, 11);
+            this.cbSheet.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbSheet.Name = "cbSheet";
+            this.cbSheet.Size = new System.Drawing.Size(208, 24);
+            this.cbSheet.TabIndex = 10;
+            // 
+            // btnImporter
+            // 
+            this.btnImporter.Location = new System.Drawing.Point(437, 12);
+            this.btnImporter.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnImporter.Name = "btnImporter";
+            this.btnImporter.Size = new System.Drawing.Size(112, 28);
+            this.btnImporter.TabIndex = 3;
+            this.btnImporter.Text = "Importar";
+            this.btnImporter.UseVisualStyleBackColor = true;
+            this.btnImporter.Click += new System.EventHandler(this.btnImporter_Click);
+            // 
+            // ImporterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(965, 498);
+            this.Controls.Add(this.cbSheet);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.lbQtdLines);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnImporter);
+            this.Controls.Add(this.cmdOk);
+            this.Controls.Add(this.gridImporter);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "ImporterForm";
+            this.Text = "Importador";
+            ((System.ComponentModel.ISupportInitialize)(this.gridImporter)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gridImporter;
+        private System.Windows.Forms.Button cmdOk;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbQtdLines;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.ComboBox cbSheet;
+        private System.Windows.Forms.Button btnImporter;
+    }
+}
+
---
+++ ImportExcel/ImporterForm.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImportExcel
+{
+    public partial class ImporterForm : Form
+    {
+        private DataSet output;
+
+        public ImporterForm()
+        {
+            InitializeComponent();
+            lbQtdLines.Text = string.Empty;
+            cbSheet.SelectedIndexChanged += CbSheet_SelectedIndexChanged;
+        }
+
+        private void cmdOk_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnImporter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var fileDialog = new OpenFileDialog();
+                fileDialog.Filter = "Excel Worksheets | *.xlsx;*.xls";
+                fileDialog.Title = "Selecione o arquivo";
+
+                if (fileDialog.ShowDialog() != DialogResult.OK) return;
+                txtFileName.Text = fileDialog.FileName;
+
+                this.Cursor = Cursors.WaitCursor;
+
+                if (!File.Exists(fileDialog.FileName))
+                    throw new Exception("Arquivo não encontrado");
+
+                var spreadsheets = GetSpreadsheets(fileDialog.FileName);
+                FillComboSheet(spreadsheets);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                this.Cursor = Cursors.Default;
+            }
+        }
+
+        private DataSet GetSpreadsheets(string fileName)
+        {
+            var strConnection64 = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={fileName};Extended Properties=\"Excel 12.0;HDR=Yes;IMEX=0\"";
+            var strConnection32 = $"Provider=Microsoft.Jet.OLEDB.4.0;Data Source={fileName};Extended Properties=\"Excel 8.0;HDR=Yes;IMEX=0\"";
+            var strConnection = Path.GetExtension(fileName) == ".xlsx" ? strConnection64 : strConnection32;
+
+            var conn = new OleDbConnection(strConnection);
+            conn.Open();
+
+            var dt = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+
+            output = new DataSet();
+            foreach (DataRow row in dt.Rows)
+            {
+                var sheet = row["TABLE_NAME"].ToString();
+                var cmd = new OleDbCommand("SELECT * FROM [" + sheet + "]", conn);
+                cmd.CommandType = CommandType.Text;
+                var outputTable = new DataTable(sheet);
+                output.Tables.Add(outputTable);
+                new OleDbDataAdapter(cmd).Fill(outputTable);
+            }
+
+            return output;
+        }
+
+        private void CbSheet_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                var table = output.Tables[cbSheet.SelectedValue.ToString()];
+                FillGrid(table);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void FillComboSheet(DataSet spreadsheets)
+        {
+            var listTable = new List<string>();
+            foreach (DataTable sheet in spreadsheets.Tables)
+                listTable.Add(sheet.TableName);
+
+            cbSheet.DataSource = listTable;
+        }
+
+        private void FillGrid(DataTable table)
+        {
+            gridImporter.DataSource = table;
+            gridImporter.AutoGenerateColumns = true;
+            lbQtdLines.Text = table.Rows.Count.ToString();
+        }
+    }
+}
